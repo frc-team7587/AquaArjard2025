38eeccd1cd54d866c0ae0efa4e3ad7e3426dc9c0
Bilol.B <113650175+Bilol135@users.noreply.github.com>
2025-02-14 08:18:46 -0500

    Intake constants added and organized

--- src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
@@ -1,10 +1,22 @@
 package frc.robot.subsystems.Intake;
 
 public class IntakeConstants {
-    public static final double kIntakeUpPosition = 9;
-    public static final double kIntakeDownPosition = 0;
-    public static final double kIntakeInSpeed = 0.5;
     public static final double kIntakeOutSpeed= -0.5;
-    public static final double kIntakeNeutralPosition = 3.7;
+    public static final double kIntakeInSpeed = 0.5;
+    public static final double kPivotSpeed = 0.5;
+
+    public static final int kIntakeMotorID = 10;
+    public static final int kPivotMotorID = 11;
+
+    public static final double kPivotMaxPosition = 0.0;
+    public static final double kPivotNeutalPosition = 0.0;
+    public static final double kPivotMinPosition = 0.0;
+
+    public static final double kP = 0.0;
+    public static final double kI = 0.0;
+    public static final double kD = 0.0;
+    public static final double kFF = 0.0;
+    public static final double kMinOutput = -1.0;
+    public static final double kMaxOutput = 1.0;
 
 }
